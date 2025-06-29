6c854e531ab4e308e82b186a6b81f1c514a8163b
jubayerahmmad <zubayersami510@gmail.com>
2025-06-30 00:08:53 +0600

    Project Initialization

---
+++ src/routes/router.jsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
+import Events from "../pages/Events/Events";
+import AddEvent from "../pages/AddEvent/AddEvent";
+import MyEvents from "../pages/MyEvents/MyEvents";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/events",
+        element: <Events />,
+      },
+      {
+        path: "/add-event",
+        element: <AddEvent />,
+      },
+      {
+        path: "/my-events",
+        element: <MyEvents />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
